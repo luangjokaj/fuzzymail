909935715625784af04246974e8c2b2011d8e408
Luan Gjokaj <luan@riangle.com>
2021-03-31 13:38:03 +0200

    fix: build

--- installer/modules/run.js
+++ installer/modules/run.js
@@ -1,15 +1,15 @@
-const fs = require("fs");
+const fs = require('fs');
 const theCWD = process.cwd();
-const theCWDArray = theCWD.split("/");
+const theCWDArray = theCWD.split('/');
 const theDir = theCWDArray[theCWDArray.length - 1];
-const ora = require("ora");
-const execa = require("execa");
-const chalk = require("chalk");
-const download = require("download");
-const handleError = require("./handleError.js");
-const clearConsole = require("./clearConsole.js");
-const printNextSteps = require("./printNextSteps.js");
-const version = require("../package.json").version;
+const ora = require('ora');
+const execa = require('execa');
+const chalk = require('chalk');
+const download = require('download');
+const handleError = require('./handleError.js');
+const clearConsole = require('./clearConsole.js');
+const printNextSteps = require('./printNextSteps.js');
+const version = require('../package.json').version;
 
 module.exports = () => {
 	// Init
@@ -54,114 +54,104 @@ module.exports = () => {
 	];
 
 	// Organise file structure
-	const dotFiles = [".gitignore", ".editorconfig"];
-	const srcFiles = ["index.html"];
+	const dotFiles = ['.gitignore', '.editorconfig'];
+	const srcFiles = ['index.html'];
 	const includesFiles = [
-		"footer.html",
-		"header.html",
-		"logo.html",
-		"single-column.html",
-		"socials.html",
-		"title.html",
-		"two-columns.html",
+		'footer.html',
+		'header.html',
+		'logo.html',
+		'single-column.html',
+		'socials.html',
+		'title.html',
+		'two-columns.html',
 	];
 	const cssFiles = [
-		"email-framework.css",
-		"fuzzy.css",
-		"globals.css",
-		"styles.css",
-		"variables.css",
+		'email-framework.css',
+		'fuzzy.css',
+		'globals.css',
+		'styles.css',
+		'variables.css',
 	];
 	const socialImgFiles = [
-		"email.png",
-		"facebook.png",
-		"instagram.png",
-		"linkedin.png",
-		"twitter.png",
+		'email.png',
+		'facebook.png',
+		'instagram.png',
+		'linkedin.png',
+		'twitter.png',
 	];
-	const imgFiles = ["header.png", "logo.png", "logo.svg"];
+	const imgFiles = ['header.png', 'logo.png', 'logo.svg'];
 
 	// Start
-	console.log("\n");
+	console.log('\n');
 	console.log(
-		"📦 ",
+		'📦 ',
 		chalk.black.bgYellow(
-			` Downloading 📨 FuzzyMail files in: → ${chalk.bgGreen(
-				` ${theDir} `,
-			)}\n`,
+			` Downloading 📨 FuzzyMail files in: → ${chalk.bgGreen(` ${theDir} `)}\n`
 		),
 		chalk.dim(`\n In the directory: ${theCWD}\n`),
-		chalk.dim("This might take a couple of minutes.\n"),
+		chalk.dim('This might take a couple of minutes.\n')
 	);
 
-	const spinner = ora({ text: "" });
+	const spinner = ora({ text: '' });
 	spinner.start(
 		`1. Creating 📨 FuzzyMail files inside → ${chalk.black.bgWhite(
-			` ${theDir} `,
-		)}`,
+			` ${theDir} `
+		)}`
 	);
 
 	// Download
 	Promise.all(filesToDownload.map((x) => download(x, `${theCWD}`))).then(
 		async () => {
-			if (!fs.existsSync("src")) {
-				await execa("mkdir", [
-					"src",
-					"src/includes",
-					"src/assets",
-					"src/assets/css",
-					"src/assets/img",
-					"src/assets/img/socialmedia",
+			if (!fs.existsSync('src')) {
+				await execa('mkdir', [
+					'src',
+					'src/includes',
+					'src/assets',
+					'src/assets/css',
+					'src/assets/img',
+					'src/assets/img/socialmedia',
 				]);
 			}
 
 			dotFiles.map((x) =>
 				fs.rename(`${theCWD}/${x.slice(1)}`, `${theCWD}/${x}`, (err) =>
-					handleError(err),
-				),
+					handleError(err)
+				)
 			);
 			srcFiles.map((x) =>
-				fs.rename(`${theCWD}/${x}`, `${theCWD}/src/${x}`, (err) =>
-					handleError(err),
-				),
+				fs.rename(`${theCWD}/${x}`, `${theCWD}/src/${x}`, (err) => handleError(err))
 			);
 			includesFiles.map((x) =>
-				fs.rename(
-					`${theCWD}/${x}`,
-					`${theCWD}/src/includes/${x}`,
-					(err) => handleError(err),
-				),
+				fs.rename(`${theCWD}/${x}`, `${theCWD}/src/includes/${x}`, (err) =>
+					handleError(err)
+				)
 			);
 			cssFiles.map((x) =>
-				fs.rename(
-					`${theCWD}/${x}`,
-					`${theCWD}/src/assets/css/${x}`,
-					(err) => handleError(err),
-				),
+				fs.rename(`${theCWD}/${x}`, `${theCWD}/src/assets/css/${x}`, (err) =>
+					handleError(err)
+				)
 			);
 			socialImgFiles.map((x) =>
 				fs.rename(
 					`${theCWD}/${x}`,
 					`${theCWD}/src/assets/img/socialmedia/${x}`,
-					(err) => handleError(err),
-				),
+					(err) => handleError(err)
+				)
 			);
 			imgFiles.map((x) =>
-				fs.rename(
-					`${theCWD}/${x}`,
-					`${theCWD}/src/assets/img/${x}`,
-					(err) => handleError(err),
-				),
+				fs.rename(`${theCWD}/${x}`, `${theCWD}/src/assets/img/${x}`, (err) =>
+					handleError(err)
+				)
 			);
 			spinner.succeed();
 
 			// The npm install
-			spinner.start("2. Installing npm packages...");
-			await execa("npm", ["install"]);
+			spinner.start('2. Installing npm packages...');
+			await execa('npm', ['install']);
 			spinner.succeed();
 
 			// Done
 			printNextSteps();
-		},
+		}
 	);
 };
