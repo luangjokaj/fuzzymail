edbfd5c30688331c4a89cd1f24c336eac1137bf6
Luan <luan@riangle.com>
2021-03-31 06:04:08 +0200

    docs: update readme

--- installer/modules/clearConsole.js
+++ installer/modules/clearConsole.js
@@ -1,11 +1,15 @@
 /**
- * Cross platform clear console.
+ * Cross platform clear console
  *
- * Support for win32 and others.
+ * Support for win32 and others
  */
 
-'use strict';
+"use strict";
 
 module.exports = () => {
-	process.stdout.write('win32' === process.platform ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H');
+	process.stdout.write(
+		"win32" === process.platform
+			? "\x1B[2J\x1B[0f"
+			: "\x1B[2J\x1B[3J\x1B[H",
+	);
 };
--- installer/modules/printNextSteps.js
+++ installer/modules/printNextSteps.js
@@ -1,58 +1,68 @@
-/**
- * Prints next steps.
- *
- * @param {string} blockName The block name.
- * @param  {string} blockDir The block directory.
- */
-
-const chalk = require('chalk');
+const chalk = require("chalk");
 
 module.exports = () => {
-	console.log('\n\n✅ ', chalk.black.bgGreen(' All done! Happy coding. \n'));
+	console.log("\n\n✅ ", chalk.black.bgGreen(" All done! Happy coding. \n"));
 	console.log(
-		'Installer has added 📨 FuzzyMail files to the current directory.  ',
-		'\nInside this directory, you can run this command:',
+		"Installer has added 📨 FuzzyMail files to the current directory.  ",
+		"\nInside this directory, you can run this command:",
 	);
 
-	// Scripts.
+	// Scripts
 	console.log(
-		'\n👉 ',
-		' Type',
-		chalk.black.bgWhite(' npm run dev '),
-		'\n\n',
-		'	Use to compile and run your files.',
-		'\n',
-		'	Watches for any changes and reports back any errors in your code.',
+		"\n👉 ",
+		" Type",
+		chalk.black.bgWhite(" npm run dev "),
+		"\n\n",
+		"	Use to compile and run your files.",
+		"\n",
+		"	Watches for any changes and reports back any errors in your code.",
 	);
 
-	// Support.
-	console.log('\n✊ ', chalk.black.bgYellow(' Support FuzzyMail \n'));
-	console.log('Like FuzzyMail? Check out our other free and open source repositories: \n');
+	// Support
+	console.log("\n✊ ", chalk.black.bgYellow(" Support FuzzyMail \n"));
 	console.log(
-		`	${chalk.yellow('WordPressify → ')} https://bit.ly/2KTqyQX`,
-		'\n',
-		`	${chalk.gray('Development workflow for WordPress themes.')}`,
-		'\n',
-		`	${chalk.yellow('GoPablo → ')} https://bit.ly/2Hgkfpy`,
-		'\n',
-		`	${chalk.gray('GoPablo is a static site generator.')}`,
-		'\n',
-		`	${chalk.yellow('ReactFondue → ')} https://bit.ly/2OXgStR`,
-		'\n',
-		`	${chalk.gray('SEO optimized React applications with SSR.')}`,
-		'\n',
-		`	${chalk.green('Powered by Riangle → ')} https://bit.ly/2P5i26I`,
-		'\n',
-		'\n',
-		`	${chalk.red('Thank you for using 📨 FuzzyMail → ')} https://www.fuzzymail.co`,
+		"Like FuzzyMail? Check out our other free and open source repositories: \n",
+	);
+	console.log(
+		`	${chalk.yellow("Cherry → ")} https://bit.ly/3sEr75P`,
+		"\n",
+		`	${chalk.gray("• A design system to build the web.")}`,
+		"\n",
+		`	${chalk.yellow("GoPablo → ")} http://bit.ly/2Hgkfpy`,
+		"\n",
+		`	${chalk.gray("• Create optimized static websites.")}`,
+		"\n",
+		`	${chalk.yellow("WordPressify → ")} https://bit.ly/2KTqyQX`,
+		"\n",
+		`	${chalk.gray("• Automate your WordPress development workflow.")}`,
+		"\n",
+		`	${chalk.yellow("Nextify → ")} https://bit.ly/3m4lVWm`,
+		"\n",
+		`	${chalk.gray("• React apps using Next.js and Emotion.")}`,
+		"\n",
+		`	${chalk.yellow("FuzzyMail → ")} https://bit.ly/2P3Irlr`,
+		"\n",
+		`	${chalk.gray("• Responsive email template generator.")}`,
+		"\n",
+		`	${chalk.green("Powered by Riangle → ")} https://bit.ly/2P5i26I`,
+		"\n",
+		"\n",
+		`	${chalk.red(
+			"Thank you for using 📨 FuzzyMail → ",
+		)} https://www.fuzzymail.co`,
 	);
 
-	// Get started.
-	console.log('\n\n🎯 ', chalk.black.bgGreen(' Get Started → \n'));
-	console.log(' You can start: \n');
+	// Get started
+	console.log("\n\n🎯 ", chalk.black.bgGreen(" Get Started → \n"));
+	console.log(" You can start: \n");
 	console.log(
-		` ${chalk.dim('1.')} Editing your new email template: ${chalk.green(`${process.cwd()}/src`)}`,
+		` ${chalk.dim("1.")} Editing your new email template: ${chalk.green(
+			`${process.cwd()}/src`,
+		)}`,
 	);
-	console.log(` ${chalk.dim('2.')} Running: ${chalk.green('npm')} run dev`, '\n\n');
+	console.log(
+		` ${chalk.dim("2.")} Running: ${chalk.green("npm")} run dev`,
+		"\n\n",
+	);
 	process.exit();
 };
